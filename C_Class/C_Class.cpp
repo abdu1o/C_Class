3e7c3c5808fc536401f044955b0f93af0ad5fef4
Oleksandr Kapara <a.kapara2006@gmail.com>
2023-03-10 18:45:44 +0200

    + try/catch

--- C_Class/C_Class.cpp
+++ C_Class/C_Class.cpp
@@ -1,81 +1,93 @@
 ﻿#include <iostream>
+#include <stdexcept>
 using namespace std;
 
 class Fraction
 {
 private:
-	int numerator;
-	int denominator;
+    int numerator;
+    int denominator;
 
 public:
-	void set(int num1, int num2)
-	{
-		numerator = num1;
-		denominator = num2;
-	}
 
-	void show()
-	{
-		cout << "Your fraction: " << numerator << '/' << denominator << "\n\n";
-	}
+    void set(int num1, int num2)
+    {
+        numerator = num1;
+        if (num2 == 0)
+        {
+            throw invalid_argument("Denominator can`t be zero!");
+        }
+        denominator = num2;
+    }
 
-	void plus(double num)
-	{
-		double solution = (numerator / denominator) + num;
-		cout << numerator << '/' << denominator << " + " << num << " = " << solution << endl;
-	}
-	void minus(double num)
-	{
-		double solution = (numerator / denominator) - num;
-		cout << numerator << '/' << denominator << " - " << num << " = " << solution << endl;
-	}
-	void division(double num)
-	{
-		double solution = numerator / (denominator * num);
-		cout << numerator << '/' << denominator << " : " << num << " = " << solution << endl;
-	}
-	void multiplication(double num)
-	{
-		double solution = (numerator * num) / denominator;
-		cout << numerator << '/' << denominator << " * " << num << " = " << solution << endl;
-	}
+    void division(double num)
+    {
+        if (num == 0)
+        {
+            throw logic_error("Numerator can`t be zero");
+        }
+        double solution = numerator / (denominator * num);
+        cout << numerator << '/' << denominator << " : " << num << " = " << solution << endl;
+    }
+
+    void show()
+    {
+        cout << "Your fraction: " << numerator << '/' << denominator << "\n\n";
+    }
+
+    void plus(double num)
+    {
+        double solution = (numerator / static_cast<double>(denominator)) + num;
+        cout << numerator << '/' << denominator << " + " << num << " = " << solution << endl;
+    }
+
+    void minus(double num)
+    {
+        double solution = (numerator / static_cast<double>(denominator)) - num;
+        cout << numerator << '/' << denominator << " - " << num << " = " << solution << endl;
+    }
+
+    void multiplication(double num)
+    {
+        double solution = (numerator * num) / static_cast<double>(denominator);
+        cout << numerator << '/' << denominator << " * " << num << " = " << solution << endl;
+    }
 };
-
-
 
 int main()
 {
-	int numerator, denominator;
-	int num;
+    int numerator, denominator;
+    int num;
 
-	cout << "Enter numerator: ";
-	cin >> numerator;
+    cout << "Enter numerator: ";
+    cin >> numerator;
 
-	cout << "Enter denominator: ";
-	cin >> denominator;
+    cout << "Enter denominator: ";
+    cin >> denominator;
 
-	if (denominator == 0)
-	{
-		while (denominator == 0)
-		{
-			cout << "Enter NORMAL denominator: ";
-			cin >> denominator;
-		}
-	}
+    try
+    {
+        Fraction object;
+        object.set(numerator, denominator);
 
-	cout << "Enter number: ";
-	cin >> num;
+        cout << "Enter number: ";
+        cin >> num;
 
-	Fraction object;
+        cout << "\n\n";
 
-	object.set(numerator, denominator);
-
-	system("cls");
-	object.show();
-
-	object.plus(num);
-	object.minus(num);
-	object.multiplication(num);
-	object.division(num);
+        object.show();
+        object.plus(num);
+        object.minus(num);
+        object.multiplication(num);
+        object.division(num);
+        object.division(0);
+    }
+    catch (const invalid_argument& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+    catch (const logic_error& e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
 }
-
